bec3c62c86e365465a61415ee7e230737eaa6d7e
halfshelldell <simmonslydell@gmai.com>
2016-06-23 15:41:54 -0400

    initial added user login/logout

--- src/main/java/com/ironyard/controllers/CrudSpringController.java
+++ src/main/java/com/ironyard/controllers/CrudSpringController.java
@@ -1,5 +1,7 @@
 package com.ironyard.controllers;
 
+import com.ironyard.entities.Sneaker;
+import com.ironyard.services.SneakerRepository;
 import com.ironyard.utilities.PasswordStorage;
 import com.ironyard.entities.User;
 import com.ironyard.services.UserRepository;
@@ -20,11 +22,21 @@ public class CrudSpringController {
     @Autowired
     UserRepository users;
 
+    @Autowired
+    SneakerRepository sneakers;
+
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String home(HttpSession session, Model model) {
         String username = (String) session.getAttribute("username");
-        model.addAttribute("username", username);
-        return "home";
+
+        if (username == null) {
+            return "home";
+        }
+        else {
+            model.addAttribute("username", username);
+            model.addAttribute("sneaker", sneakers.findAll());
+            return "sneaker";
+        }
     }
 
     @RequestMapping(path = "/login", method = RequestMethod.POST)
@@ -46,6 +58,23 @@ public class CrudSpringController {
     @RequestMapping(path = "/logout", method = RequestMethod.POST)
     public String logout(HttpSession session) {
         session.invalidate();
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/create-sneaker", method = RequestMethod.POST)
+    public String createSneaker(HttpSession session, String brand, String name, int year, float price, int size) {
+        String username = (String) session.getAttribute("username");
+        User user = users.findByName(username);
+        Sneaker sneaker = new Sneaker(brand, name, year, price, size, user);
+        sneakers.save(sneaker);
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "delete-sneaker", method = RequestMethod.POST)
+    public String deleteSneaker(HttpSession session, int id) {
+        String username = (String) session.getAttribute("username");
+        if (!users.findByName()username.equals(username))
+        sneakers.delete(id);
         return "redirect:/";
     }
 
